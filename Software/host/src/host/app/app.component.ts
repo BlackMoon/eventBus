9b34970b5e3826da56f879daa2e107ee28f3a900
Роман Романов <romanovrv@aquilon-st.ru>
2017-02-07 14:22:28 +0300

    app.route

--- Software/host/src/host/app/app.component.ts
+++ Software/host/src/host/app/app.component.ts
@@ -3,7 +3,7 @@
 @Component({
     selector: 'eventBus-app',
     styles: [require('to-string!./app.component.css')],
-    template: require('to-string!./app.component.html')
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
     title = "Angular 2 Test";
--- Software/host/src/host/app/vendor.ts
+++ Software/host/src/host/app/vendor.ts
@@ -12,3 +12,5 @@ import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
 import 'jquery';
+import 'jqueryui';
+import 'igniteui-angular2';
