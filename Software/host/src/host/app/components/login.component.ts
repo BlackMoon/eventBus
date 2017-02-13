7b5969e98094b08be16c44f224617bb9e352440e
Роман Романов <romanovrv@aquilon-st.ru>
2017-02-13 17:11:49 +0300

    login.combinator

---
+++ Software/host/src/host/app/components/login.component.ts
@@ -0,0 +1,40 @@
+﻿import { Component, AfterViewInit } from '@angular/core';
+
+declare var $: any;
+
+@Component({
+    selector: 'app-login',
+    templateUrl: 'login.component.html'
+})
+
+export class LoginComponent implements AfterViewInit {   
+
+    private options: any;     
+
+    constructor() {
+        this.options = {
+            headerText: "Войти в систему",
+            modal: true,
+            openAnimation: "slide",
+            resizable: false,
+            showCloseButton: false,
+            showFooter: true,
+            height: 300,
+            width: 400,
+            state: "closed"
+        }
+    }    
+
+    ngAfterViewInit() {
+        $("input[type=submit]").button();
+        $("input[type=reset]").button();
+    }
+
+    show() {        
+        this.options.state = 'opened';
+    }
+
+    hide() {
+        this.options.state = 'closed';
+    }
+}
