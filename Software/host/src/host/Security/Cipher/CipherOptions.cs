c7fcff5fdb9faf5f276376f00b73d7d785569e4a
Роман Романов <romanovrv@aquilon-st.ru>
2017-03-01 16:44:35 +0300

    Cipher

---
+++ Software/host/src/host/Security/Cipher/CipherOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace Host.Security.Cipher
+{
+    /// <summary>
+    /// Cipher options
+    /// </summary>
+    public class CipherOptions
+    {
+        /// <summary>
+        /// Key size: in bits
+        /// </summary>
+        public byte KeySize { get; set; } = 128;
+
+        /// <summary>
+        /// Cipher algorithm
+        /// </summary>
+        public string Algorithm { get; set; } = "AES";
+
+        /// <summary>
+        /// The relative request path to listen on.
+        /// </summary>
+        /// <remarks>The default path is <c>/secret</c>.</remarks>
+        public string Path { get; set; } = "/secret";
+    }
+}
---
+++ Software/host/src/host/Security/SecretItem.cs
@@ -0,0 +1,18 @@
+﻿using Kit.Core.Web.Mvc.Converters;
+using Newtonsoft.Json;
+
+namespace Host.Security
+{
+    /// <summary>
+    /// Единица безопасности
+    /// </summary>
+    public class SecretItem
+    {
+        public string Algorithm { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+       
+        public byte[] Key { get; set; }
+    }
+}
