618fa888e372db5c3453d9729f0480c740344736
Роман Романов <romanovrv@aquilon-st.ru>
2017-03-15 17:12:10 +0300

    evtBus

--- Software/host/src/domain/AdkUser/Query/AdkUserQueryHandler.cs
+++ Software/host/src/domain/AdkUser/Query/AdkUserQueryHandler.cs
@@ -1,21 +1,26 @@
-﻿using System;
-using Dapper;
+﻿using Dapper;
 using Kit.Core.CQRS.Query;
 using Kit.Dal.DbManager;
 
 namespace domain.AdkUser.Query
 {
-    public class AdkUserQueryHandler : IQueryHandler<FindUserByIdQuery, AdkUser>
+    public class AdkUserQueryHandler : IQueryHandler<FindUserByIdQuery, AdkUser>, IQueryHandler<FindUserByLoginQuery, AdkUser>
     {
         private readonly IDbManager _dbManager;
         public AdkUserQueryHandler(IDbManager dbManager)
         {
             _dbManager = dbManager;
+            _dbManager.Open();
         }
 
         public AdkUser Execute(FindUserByIdQuery query)
         {
-            return _dbManager.DbConnection.QuerySingle<AdkUser>("WHERE id = @id", new { id = query.Id });
+            return _dbManager.DbConnection.QuerySingle<AdkUser>("SELECT * FROM adk_user.users u WHERE u.id = @id", new { id  = query.Id });
+        }
+
+        public AdkUser Execute(FindUserByLoginQuery query)
+        {
+            return _dbManager.DbConnection.QuerySingle<AdkUser>("SELECT * FROM adk_user.users u WHERE u.login = @login", new { id = query.Login });
         }
     }
 }
