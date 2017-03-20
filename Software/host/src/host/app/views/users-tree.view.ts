94d085126159b1637f6a4eab1f220c9145149c34
Роман Романов <romanovrv@aquilon-st.ru>
2017-03-20 17:32:04 +0300

    users-tree.view.html

---
+++ Software/host/src/host/app/views/users-tree.view.ts
@@ -0,0 +1,30 @@
+﻿import { Component } from '@angular/core';
+import { RoutingComponent } from '../navigation/route.decorator';
+
+@Component({          
+    templateUrl: 'users-tree.view.html'
+})
+@RoutingComponent()
+export class UsersTreeView {
+
+    private id: string;
+    private tgridOptions: any;
+
+    constructor() {
+        
+        this.tgridOptions = {
+            autoCommit: true,
+            dataSource: [{ "id": 0 }, { "id": 1 }, { "id": 2 }],
+            width: "100%",
+            height: "400px",
+            autoGenerateColumns: false,
+            autoGenerateColumnLayouts: false,
+            primaryKey: "id",
+            childDataKey: "products",
+            renderExpansionIndicatorColumn: true,
+            columns: [
+                { key: "id", headerText: "ID", width: "100px", dataType: "number" }                
+            ]
+        };
+    }
+}
---
+++ Software/host/src/host/app/views/views.module.ts
@@ -0,0 +1,9 @@
+﻿import { NgModule } from "@angular/core";
+import { IgTreeGridComponent } from 'igniteui-angular2/igniteui.angular2';
+import * as views from './index';
+
+@NgModule({
+    declarations: [IgTreeGridComponent, views.MonitorView, views.PageNotFoundView, views.UsersTreeView],
+    exports: [IgTreeGridComponent, views.MonitorView, views.PageNotFoundView, views.UsersTreeView]    
+})
+export class ViewsModule { }
