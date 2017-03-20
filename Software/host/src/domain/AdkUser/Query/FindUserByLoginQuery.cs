4e1d1c1579ba128d44b78820b3c92b6dbfc25a43
Роман Романов <romanovrv@aquilon-st.ru>
2017-03-20 09:24:42 +0300

    findUserByLoginQuery

---
+++ Software/host/src/domain/AdkUser/Query/FindUserByLoginQuery.cs
@@ -0,0 +1,13 @@
+﻿using Kit.Core.CQRS.Query;
+
+namespace domain.AdkUser.Query
+{
+    /// <summary>
+    /// Запрос. Найти пользователя по login'у
+    /// </summary>
+    public class FindUserByLoginQuery : IQuery
+    {
+        public string ConnectionString { get; set; }
+        public string Login { get; set; }
+    }
+}
