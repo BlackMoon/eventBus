f8a648a8949bdbb1547036a19a5f9e18d9ed1674
Роман Романов <romanovrv@aquilon-st.ru>
2017-03-27 16:56:44 +0300

    comments

--- Software/host/src/domain/Common/Query/FindGroupChildrenQuery.cs
+++ Software/host/src/domain/Common/Query/FindGroupChildrenQuery.cs
@@ -1,5 +1,8 @@
 ﻿namespace domain.Common.Query
 {
+    /// <summary>
+    /// Запрос поиска потомков группы
+    /// </summary>
     public class FindGroupChildrenQuery
     {
         public string GroupId { get; set; }
---
+++ Software/host/src/domain/Common/Query/GetAllQuery.cs
@@ -0,0 +1,11 @@
+﻿using Kit.Core.CQRS.Query;
+
+namespace domain.Common.Query
+{
+    /// <summary>
+    /// Запрос поиска всех объектов
+    /// </summary>
+    public class GetAllQuery : IQuery
+    {
+    }
+}
